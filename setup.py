699d5e5579eac595add2d5f664696c2c0ddad3af
Shane Drury <shane.r.drury@gmail.com>
2015-02-16 09:39:09 +0000

    Initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='gametest',
+    version='',
+    packages=['gametest', 'gametest.lib'],
+    url='',
+    license='',
+    author='Shane Drury',
+    author_email='shane.r.drury@gmail.com',
+    description='',
+    install_requires=['pyyaml']
+)
